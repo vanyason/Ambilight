43f1d3c9439f7caa3fdf4dd227879e75313f89d9
Ivan <vanyason@mail.ru>
2018-06-20 13:23:34 +0300

    little corrections since last commit

--- QT_PC_controller/qController/lightmusicmode.cpp
+++ QT_PC_controller/qController/lightmusicmode.cpp
@@ -36,27 +36,14 @@ QColor Mode2::handleSoundLevel(qreal level)
 
     if (level > 0 && level <= 0.3)
         color.setRgb(255 - 850 * level, 0, 255);
-
-
     else if (level > 0.3 && level <= 0.5)
         color.setRgb(0, 1275 * -(0.3 - level), 255);
-
-
-
     else if (level > 0.5 && level <= 0.7)
         color.setRgb(0, 255, 255 - (1275 * -(0.5 - level)));
-
-
-
     else if (level > 0.7 && level <= 0.9)
         color.setRgb(1275 * -(0.7 - level), 255, 0);
-
-
     else if (level > 0.9 && level <= 1)
         color.setRgb(level * 255, 0, 0);
-
-
-
 
     return color;
 }
